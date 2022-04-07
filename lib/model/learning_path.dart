d2a1e5ca4c3417ed05c72fce427710d930a8c75a
Aflah Azzaky <aflahazzaki123@gmail.com>
2022-04-08 01:14:00 +0700

    Add a shortName

--- lib/model/learning_path.dart
+++ lib/model/learning_path.dart
@@ -1,11 +1,13 @@
 class LearningPath {
-  String name;
+  String fullName;
+  String shortName;
   String description;
   String imageAsset;
   String jumlahKelas;
 
   LearningPath({
-    required this.name,
+    required this.fullName,
+    required this.shortName,
     required this.description,
     required this.imageAsset,
     required this.jumlahKelas,
@@ -14,56 +16,64 @@ class LearningPath {
 
 var LearningPathList = [
   LearningPath(
-    name: 'Learning Path Android Developer',
+    fullName: 'Learning Path Android Developer',
+    shortName: 'Android Developer',
     description:
         'Kurikulum didesain dengan persetujuan dari Tim Google Android untuk mempersiapkan developer Android standar Global. Dicoding adalah Google Developer Authorized Training Partner.',
     imageAsset: 'images/android_developer.png',
     jumlahKelas: '6 Kelas',
   ),
   LearningPath(
-    name: 'Learning Path Azure Cloud Developer',
+    fullName: 'Learning Path Azure Cloud Developer',
+    shortName: 'Azure Cloud Developer',
     description:
         'Kurikulum lengkap yang didesain bersama salah satu penyedia cloud solution terbesar di dunia yaitu Microsoft Azure. Setiap kelas mempersiapkan siswa untuk ujian sertifikasi resmi Azure dari Microsoft.',
     imageAsset: 'images/azure_cloud_developer.png',
     jumlahKelas: '2 Kelas',
   ),
   LearningPath(
-    name: 'Learning Path Back-End Web Developer',
+    fullName: 'Learning Path Back-End Web Developer',
+    shortName: 'Back-End Web Developer',
     description:
         'Kurikulum disusun oleh Dicoding bersama AWS beserta pelaku industri Back-End Development. Siswa dipersiapkan untuk menjadi Back-End Developer sesuai kebutuhan industri.',
     imageAsset: 'images/back_end_developer.png',
     jumlahKelas: '6 Kelas',
   ),
   LearningPath(
-    name: 'Learning Path Front-End Web Developer',
+    fullName: 'Learning Path Front-End Web Developer',
+    shortName: 'Front-End Web Developer',
     description:
         'Kurikulum disusun oleh Dicoding dan pelaku industri di bidang Web Development. Siswa dipersiapkan untuk menjadi Front-End Web Developer sesuai standar kebutuhan industri.',
     imageAsset: 'images/front_end_web_developer.png',
     jumlahKelas: '4 Kelas',
   ),
   LearningPath(
-    name: 'Learning Path Google Cloud Developer',
+    fullName: 'Learning Path Google Cloud Developer',
+    shortName: 'Google Cloud Developer',
     description:
         'Kurikulum didesain oleh Dicoding bersama Google dan para pelaku industri di bidang Cloud Computing. Siswa dipersiapkan untuk menjadi Google Cloud Professional sesuai kebutuhan industri.',
     imageAsset: 'images/google_cloud_developer.png',
     jumlahKelas: '5 Kelas',
   ),
   LearningPath(
-    name: 'Learning Path iOS Developer',
+    fullName: 'Learning Path iOS Developer',
+    shortName: 'iOS Developer',
     description:
         'Kurikulum disusun oleh Dicoding dan pelaku industri di bidang iOS Development. Siswa dipersiapkan untuk menjadi iOS Developer sesuai standar kebutuhan industri.',
     imageAsset: 'images/ios_developer.png',
     jumlahKelas: '5 Kelas',
   ),
   LearningPath(
-    name: 'Learning Path Machine Learning Developer',
+    fullName: 'Learning Path Machine Learning Developer',
+    shortName: 'Machine Learning Developer',
     description:
         'Kurikulum lengkap yang disusun oleh Dicoding bersama IBM dan Google beserta pelaku industri. Siswa dipersiapkan untuk menjadi Machine Learning Developer sesuai standar kebutuhan industri.',
     imageAsset: 'images/machine_learning_developer.png',
     jumlahKelas: '5 Kelas',
   ),
   LearningPath(
-    name: 'Learning Path Multi Platfrom App Developer',
+    fullName: 'Learning Path Multi Platfrom App Developer',
+    shortName: 'Multi Platfrom App Developer',
     description:
         'Kurikulum disusun oleh Dicoding bersama Google beserta pelaku industri Multi-Platform App Development. Siswa dipersiapkan untuk menjadi Multi-Platform App Developer sesuai standar kebutuhan industri.',
     imageAsset: 'images/multi_platform_app_developer.png',
